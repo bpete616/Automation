168ac8a466ec5944f130e921e110943043655262
BeccaQA <rebecca.peterson7798@stu.mtec.edu>
2025-10-30 22:36:25 -0600

    Object page files

--- secure.page.js
+++ secure.page.js
@@ -1,9 +1,7 @@
 import { $ } from '@wdio/globals'
 import Page from './page.js';
 
-/**
- * sub page containing specific selectors and methods for a specific page
- */
+
 class InventoryPage {
     get InventoryContainer() { return $('#inventory_container'); }
 
